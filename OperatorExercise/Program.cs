2527cae4304bb568ecaf5eac216c261d05f58bfc
taylor Schuchter <taylorschuchter@gmail.com>
2023-01-24 17:47:14 -0500

    area of circle feature added

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace OperatorExercise
 {
     public class Program
-    {
-        static void Main(string[] args)
+    { public static double AreaOfCircle(double radius)
         {
-            Console.WriteLine("Hello, World!");
+            double circleArea = Math.PI * Math.Pow(radius, 2);
+            return circleArea;
+        }
+        static void Main(string[] args)
+        {// EXERCISE ONE
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4);
+            int addition = a + b;
+            int subtraction = a - b;
+            int multiplication = a * b;
+            int modulus = a % b;
+            { Console.WriteLine($"{a}/{b} is {quotient} with a remainder of {remainder}"); }
+
+            //EXERCISE TWO
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            double circleArea = AreaOfCircle(radius);
+            Console.WriteLine($"The area of your circle is {circleArea}");
         }
     }
 }
